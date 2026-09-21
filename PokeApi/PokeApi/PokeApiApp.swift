248f6d5f601fcb15ef4d9842812cf0ff61e3cbd0
Sakamoto <cc25151@g.unicamp.br>
2026-09-21 09:17:12 -0300

    commit inicial

---
+++ PokeApi/PokeApi/PokeApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeApiApp.swift
+//  PokeApi
+//
+//  Created by PEDRO HENRIQUE SAKAMOTO MENDES on 21/09/26.
+//
+
+import SwiftUI
+
+@main
+struct PokeApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
